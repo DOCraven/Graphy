7112d314dec993642d32c9b573997032c7552704
DOCraven <brg.tman@gmail.com>
2020-04-02 00:45:25 +1100

    Initial lamba functionalility

--- Graphy.py
+++ Graphy.py
@@ -1,9 +1,14 @@
 ### THIS SCRIPT WILL MANIPULATE THE DATA, returning graphable data in the following formats
 #
-#
-#
-#
-#
+# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.resample.html
+# https://pandas.pydata.org/pandas-docs/stable/reference/resampling.html
+# https://stackoverflow.com/questions/36681945/group-dataframe-in-5-minute-intervals
+# https://stackoverflow.com/questions/17001389/pandas-resample-documentation/17001474#17001474
+# https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html
+# https://pandas.pydata.org/pandas-docs/stable/user_guide/visualization.html#visualization-scatter-matrix
+
+
+
 
 
 ### V0.1.0
@@ -12,59 +17,54 @@
 
 import pandas as pd
 import datetime as dt
-
+import matplotlib.pyplot as plt
+from pandas.plotting import scatter_matrix
 
 ### FUNCTIONS ###
 
 def xlsxReader(input): 
     """reads a given file and returns a df"""
-    ## names 
-    output = pd.read_excel(input, index_col = None) #no index for later splitting 
-    output.set_index('Interval End', drop = False, inplace = True) #set index, and keeps OG date 
-
-
-    return output
-
-def monthSplitter(df, year = 2019): 
-    """ splits the given DF into months, saves each month as a new sheet"""
+    ## read the data 
+    data = pd.read_excel(input, parse_dates = True, index_col = 0) #parsed dates 
+    data2 = data.iloc[:,0:2]
+    # reduced = data[data['VBBB00026601 - Mill Street WODONGA - kWh Consumption']>0.5] # ie. cumulative 30 minute intervals per day that the plant is running
     
-    months_19 = ['JAN_19', 'FEB_19', 'MAR_19', 'APR_19', 'MAY_19', 'JUN_19', 'JUL_19', 'AUG_19', 'SEP_19', 'OCT_19', 'NOV_19', 'DEC_19'] #names of the month (2019 flavour)
-    months_20 = ['JAN_20', 'FEB_20', 'MAR_20', 'APR_20', 'MAY_20', 'JUN_20', 'JUL_20', 'AUG_20', 'SEP_20', 'OCT_20', 'NOV_20', 'DEC_20'] #names of the month (2019 flavour)
 
-    df['Interval End'] = pd.to_datetime(df['Interval End']) #magic happens here, ripped straight from SO - https://stackoverflow.com/questions/55974955/splitting-in-pandas-a-timestamp-date/55975035
+
+    # daily = reduced.resample('1D').count() #split the new DF into daily mean 
+    sean = data2.resample("1D").sum() #sum each days demand 
+
+
+    sean2 = sean.apply(lambda x: x.div(x.max()))
     
-    if year == 2019: 
-        for number in range(0, 12): #iterate from JAN (1) to DEC (12)
-            months_19[number] = df[df['Interval End'].dt.month == (number + 1)] #adding +1 to number as months dont start
+    sean2.plot(title = 'Sean MAX')
+    plt.show()
 
-    elif year == 2020: 
-        for number in range(0, 12): #iterate from JAN (1) to DEC (12)
-            months_20[number] = df[df['Interval End'].dt.month == (number + 1)] #adding +1 to number as months dont start
+    # daily.plot(title = 'Daily Mean' ) #create the 
+    
+    
+    # data.plot(x = 'Interval End', y = 'VBBB00026601 - Mill Street WODONGA - kWh Consumption', title = 'Dave and Sean' )
+    # fig.savefig("test.png")
+    
+    # plt.show()
 
-    ## save DFS as sheets into a xlsx spreadhsheet
-
-
-
-
-
-
-
+    # daily.to_excel('Daily Mean.xlsx')
     return #nothing
+
 
 
 def main():
     """ Main fcn"""
-
+    plt.close('all')
     ### STEP 1 - read all xlsx and save as df
     name19 = 'Large Market Interval Data - March 01 2018- Feb 29 2019.xls' #2018/19 data
-    name20 = 'Large Market Interval Data - March 01 2019 -March 01 2020.xls' #2019/20 data
+    # name20 = 'Large Market Interval Data - March 01 2019 -March 01 2020.xls' #2019/20 data
     
-    DATA19 = xlsxReader(name19) 
-    DATA20 = xlsxReader(name20)
+    xlsxReader(name19) 
+    # xlsxReader(name20)
 
     ### STEP 2 - split each df into monthly sheets
-    monthSplitter(DATA19, 2019)
-    monthSplitter(DATA20, 2020)
+
 
     ### STEP 3 - Average half hourly usage by month
 
