9a641895f73c536bdeb917133f22019c73922d33
DOCraven <brg.tman@gmail.com>
2020-04-01 19:29:08 +1100

    Initial Commit

---
+++ Graphy.py
@@ -0,0 +1,26 @@
+### THIS SCRIPT WILL MANIPULATE THE DATA, returning graphable data in the following formats
+#
+#
+#
+#
+#
+
+
+### V0.1.0
+
+### libraries
+
+import pandas as pd
+
+
+### FUNCTIONS ###
+
+
+
+
+
+
+
+
+
+
