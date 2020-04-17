76b23eb2b2c09f23b66ad5663b5482d20a36034f
DOCraven <brg.tman@gmail.com>
2020-04-17 12:35:19 +1000

    Added in Monthly to Day Sum (seans magic lambda fcn)

--- Graphy.py
+++ Graphy.py
@@ -21,6 +21,9 @@
 # overlaw solar generation for each individual load to determine best time to turn things on 
 # as per drawn image in teams 
 
+## TO DO ##
+# make the plotter nicer, currently its a bit hacked together
+
 
 
 ### V0.1.0
@@ -55,35 +58,68 @@ def xlsxReader(input):
     return months
 
 def MonthToDay(df):
-    """Takes 30 minute interval data over the month, and returns an average for a specific day
+    """Takes 30 minute interval data over the month, and returns an average for a specific day. Returned data is a list of DFs. 
     """
-    sampled = []
+    sampled = [] #empty array
     for x in range (0, 12): # sets each DF to have the correct index
         df[x] = df[x].set_index(['Interval End']) #set the index, as previous DF did not have have an index
         df[x].index = pd.to_datetime(df[x].index, unit='s') # some magic to make it not error out - 
-        sampled.append(df[x].groupby([df[x].index.hour, df[x].index.minute]).mean()) #sum each days demand 
+        sampled.append(df[x].groupby([df[x].index.hour, df[x].index.minute]).mean()) #sum each days demand, returns the mean of the hours over the month 
             #REFERENCE FOR THIS https://stackoverflow.com/questions/30579950/grouping-by-30-minutes-space-in-pandas
         
-        
-        ### PROBLEM IS HERE ### NEED TO CONVERT IT TO AVERAGE HOURLY DATA FOR THE ENTIRE MONTH
-
-    ### TESTING ###
-
-
     return sampled
 
-def Plotter(df, Title = 'DAILY MEAN', XTITLE = 'Time', YTITLE = 'kWh'):
+def MonthSUM(df):
+    """
+    Takes 30 minute interval data over the month, and returns a sum for a each day
+    """
+    sampled = [] #empty array
+    for x in range (0, 12): # sets each DF to have the correct index
+        df[x] = df[x].set_index(['Interval End']) #set the index, as previous DF did not have have an index
+        df[x].index = pd.to_datetime(df[x].index, unit='s') # some magic to make it not error out - 
+        sampled.append(df[x].groupby([df[x].index.hour, df[x].index.minute]).sum()) #sum each days demand 
+            #REFERENCE FOR THIS https://stackoverflow.com/questions/30579950/grouping-by-30-minutes-space-in-pandas
+    
+    return sampled
+
+def MonthToDaySum(df): #fix it up a bit, make it nicer, as it doesnt really work. DISREGARD
+    """
+    Sums up the entire DF for the month, returns a total energy consumption for each day of the month, as a % of the highest load in each site
+    """
+    sampled = [] #empty array
+    for x in range (0, 12): # sets each DF to have the correct index
+        SUMMED = df[x].resample("1D").sum() #sum each days demand 
+        SUMMED = df[x].apply(lambda x: x.div(x.max())) #magic lambda function from Sean, divdes X by the max of X, making it into percentages
+        sampled.append(SUMMED) #append to all 
+            
+    return sampled
+
+def Plotter(df, Title = 'DAILY MEAN', XTITLE = 'Time', YTITLE = 'kWh', KIND = 'Subplot'): #this is where things are plotted, will require _some_ manual manipulation until I make it nicer
     """ Plots the given dataframe using cufflinks
     Will require manual manipulation
     """
-    
+    ## this is not very well optimised. Will fix/change it later
+    rows = 17
+    columns = 2
  
     #individual plot
     # fig = df.iplot(asFigure=True, kind = 'box')
     #big boii subplots (ie, 34 graphs)
-    # fig = df.iplot(asFigure=True, subplots=True, shape=(4,9), shared_xaxes=True, fill=True, xTitle = XTITLE, yTitle = YTITLE, title = Title) #17 2 is best for portrait monitors, 4 9 is best for landscape monitors
+    if KIND == 'Subplot':
+        fig = df.iplot(asFigure=True, subplots=True, shape=(rows, columns), shared_xaxes=True, fill=True, xTitle = XTITLE, yTitle = YTITLE, title = Title) #17 2 is best for portrait monitors, 4 9 is best for landscape monitors
     #nice looking individual plot
-    fig = df.iplot(asFigure=True, subplots=True, shared_xaxes=True, fill=True, xTitle = XTITLE, yTitle = YTITLE, title = Title) #indivual 
+    elif KIND == False:
+        # fig = df.iplot(asFigure=True, subplots=True, shared_xaxes=True, fill=True, xTitle = XTITLE, yTitle = YTITLE, title = Title) #indivual 
+        fig = df.iplot(asFigure=True, xTitle=XTITLE, yTitle=YTITLE, title=Title)
+    elif KIND == 'Bar':
+        fig = df.iplot(asFigure=True, xTitle=XTITLE, yTitle=YTITLE, title=Title, kind = 'bar')
+    
+
+
+    else:
+        print('Incorrect Plot Type')
+
+
     fig.show()
 
     return #nothing
@@ -94,18 +130,31 @@ def main():
     """ Main fcn"""
     plt.close('all')
     ### STEP 1 - read all xlsx and save as monthly df
+        ## VARS
     name19 = 'Large Market Interval Data - March 01 2018- Feb 29 2019.xls' #2018/19 data
     # name20 = 'Large Market Interval Data - March 01 2019 -March 01 2020.xls' #2019/20 data
     
+        ## READING XLS
     NEW_2019 = xlsxReader(name19) #split into months, access via indexing 
 
-    ### STEP 2 - Resample down to 30 hourly data
+    ### STEP 2 - Playing with the data
 
+    
+    ## DAILY AVERAGE PER MONTH
     NEW_2019_DAILY_MEAN = MonthToDay(NEW_2019)
-    Plotter(NEW_2019_DAILY_MEAN[0].iloc[:,0], 'JANUARY DAILY MEAN')
-    # Plotter(NEW_2019_DAILY_MEAN[0],  'JANUARY DAILY MEAN') #ie, gets the mean of every hour and averages it over a month
-  
 
+    ## MONTHLY CONSUMPTION
+    # NEW_2019_MONTHLY_CONSUMPTION = MonthToDaySum(NEW_2019) ## DISREGARD, CURRENTLY BROKEN
+    
+    ## MONTH TO DAILY SUM
+    # NEW_MONTHLY_DAILY_SUM = MonthSUM(NEW_2019)
+    
+    
+    ### PLOTTING NICE GRAPHS ###
+    
+    Plotter(NEW_2019_DAILY_MEAN[0], 'JANUARY DAILY MEAN', KIND = 'Subplot') #change kind to determine what graph is used. 
+    # Plotter(NEW_2019_MONTHLY_CONSUMPTION[0], 'JANUARY SUM', KIND = 'Box') ## DISREGARD, CURRENTLY BROKEN
+    # Plotter(NEW_MONTHLY_DAILY_SUM[0], 'JANUARY DAILY SUM', KIND = 'Bar')
 
 
 
